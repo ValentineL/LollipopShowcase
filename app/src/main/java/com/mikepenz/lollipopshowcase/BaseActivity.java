c97e9e9f5c08424076d2910ad3b33908445858ce
ValentineL <valentine.longinidis@gmail.com>
2015-10-11 23:53:55 +0300

    10.11.2015

--- app/src/main/java/com/mikepenz/lollipopshowcase/BaseActivity.java
+++ app/src/main/java/com/mikepenz/lollipopshowcase/BaseActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -25,6 +26,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.HttpRequest;
 
 
 public class BaseActivity extends AppCompatActivity {
@@ -45,40 +49,18 @@ public class BaseActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(Void... params) {
+
+
             try {
-                //Create an HTTP client
-                HttpClient client = new DefaultHttpClient();
-                HttpPost post = new HttpPost(SERVER_URL);
-                post.addHeader("X-Mashape-Key", "vn2KuqrU0omshwmhyzSpBtYiiAk7p1zDat7jsn51QzyIraeTe3");
 
-                //Perform the request and check the status code
-                HttpResponse response = client.execute(post);
-                StatusLine statusLine = response.getStatusLine();
-                if(statusLine.getStatusCode() == 200) {
-                    HttpEntity entity = response.getEntity();
-                    InputStream content = entity.getContent();
+                HttpRequest request = Unirest.get(SERVER_URL)
+                        .header("X-Mashape-Key", "vn2KuqrU0omshwmhyzSpBtYiiAk7p1zDat7jsn51QzyIraeTe3");
+                HttpResponse<JsonNode> jsonResponse = request.asJson();
+                Gson gson = new Gson();
+                String responseJSONString = jsonResponse.getBody().toString();
+                Gson myObject = gson.fromJson(responseJSONString, new TypeToken<Collection<SearchResponse>>() {
+                }.getType());
 
-                    try {
-                        //Read the server response and attempt to parse it as JSON
-                        Reader reader = new InputStreamReader(content);
-
-                        GsonBuilder gsonBuilder = new GsonBuilder();
-
-                        Gson gson = gsonBuilder.create();
-
-                        Collection<SearchResponse> searchResponses = gson.fromJson(reader, new TypeToken<Collection<SearchResponse>>() {
-                        }.getType());
-                        content.close();
-
-
-                    } catch (Exception ex) {
-                        Log.e(TAG, "Failed to parse JSON due to: " + ex);
-
-                    }
-                } else {
-                    Log.e(TAG, "Server responded with status code: " + statusLine.getStatusCode());
-
-                }
             } catch(Exception ex) {
                 Log.e(TAG, "Failed to send HTTP POST request due to: " + ex);
 
