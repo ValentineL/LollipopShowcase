77963c074630410059510c41cf240c742dc72b9f
ValentineL <valentine.longinidis@gmail.com>
2015-10-12 20:47:16 +0300

    10.12.2015

--- app/src/main/java/com/mikepenz/lollipopshowcase/BaseActivity.java
+++ app/src/main/java/com/mikepenz/lollipopshowcase/BaseActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -45,7 +48,7 @@ public class BaseActivity extends AppCompatActivity {
 
         private static final String TAG = "SimpleTask";
 
-        public static final String SERVER_URL = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/qualities/Legendary?collectible=1&locale=enUS";
+       /* public static final String SERVER_URL = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/qualities/Legendary?collectible=1&locale=enUS";*/
 
         @Override
         protected String doInBackground(Void... params) {
@@ -53,13 +56,49 @@ public class BaseActivity extends AppCompatActivity {
 
             try {
 
-                HttpRequest request = Unirest.get(SERVER_URL)
-                        .header("X-Mashape-Key", "vn2KuqrU0omshwmhyzSpBtYiiAk7p1zDat7jsn51QzyIraeTe3");
-                HttpResponse<JsonNode> jsonResponse = request.asJson();
-                Gson gson = new Gson();
-                String responseJSONString = jsonResponse.getBody().toString();
-                Gson myObject = gson.fromJson(responseJSONString, new TypeToken<Collection<SearchResponse>>() {
-                }.getType());
+
+
+
+
+                    String url = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/qualities/Legendary?collectible=1&locale=enUS";
+
+                    URL obj = new URL(url);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    //add request header
+                    con.setRequestProperty("X-Mashape-Key", "vn2KuqrU0omshwmhyzSpBtYiiAk7p1zDat7jsn51QzyIraeTe3");
+
+                    // optional default is GET
+                    con.setRequestMethod("GET");
+
+
+
+
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+
+                    Gson gson = new Gson();
+                    String responseJSONString = response.toString();
+                    SearchResponse myObject = gson.fromJson(responseJSONString, new TypeToken<Collection<SearchResponse>>() {
+                    }.getType());
+                    in.close();
+
+                    //print result
+                    System.out.println(response.toString());
+
+
+
+
+
 
             } catch(Exception ex) {
                 Log.e(TAG, "Failed to send HTTP POST request due to: " + ex);
